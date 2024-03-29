0c8e131f83784d6faeee83f82402aa7468163b38
Javier de la Rosa <versae@gmail.com>
2024-03-29 00:29:34 +0000

    Add training resuming script to crontab

--- pretrain_llama_control_resume.sh
+++ pretrain_llama_control_resume.sh
@@ -6,15 +6,26 @@
 # TPU specific flags to improve training throughput
 export LIBTPU_INIT_ARGS='--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000 --xla_tpu_spmd_rewrite_einsum_with_reshape=true --xla_enable_async_all_gather=true --jax_enable_async_collective_offload=true --xla_tpu_enable_latency_hiding_scheduler=true TPU_MEGACORE=MEGACORE_DENSE'
 
+# Add this line to crontab
+# @reboot cd /home/javierr/nb-easylm && bash pretrain_llama_control_resume.sh
+echo "Retrieving latest checkpoint..."
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/google-cloud-sdk/bin
+HOME=/home/javierr
+BOTO_CONFIG="/etc/boto.cfg"
+# CHECKPOINT=`gsutil ls -l 'gs://mimir-train-us/llama-control-checkpoint/' | sort -k 2 |head -1|xargs`
+CHECKPOINT=`PREV=$(pwd) && cd $HOME && gsutil ls -l gs://mimir-train-us/llama-control-checkpoint/** | sort -k 2 | tail -2 | head -1 | cut -d " " -f 5| cut -d "/" -f 1-5|tr -d '[[:space:]]+'  && cd $PREV`  # cd to avoid folder permissions issue
+# CHECKPOINT="gs://mimir-train-us/llama-control-checkpoint/b8e6be388b4b4706b8c8d046bfcd50fc"
+
 BATCH_SIZE=256
 SEQ_LENGTH=2048
 TOTAL_TOKENS=65748775870
 EPOCHS=2
 STEPS=`python -c "print(round($EPOCHS * $TOTAL_TOKENS / ( $SEQ_LENGTH * $BATCH_SIZE )))"`
-echo "Training for ($EPOCHS epochs * $TOTAL_TOKENS tokens / ($SEQ_LENGTH seq length * $BATCH_SIZE batch size)) = $STEPS steps"
+echo "Training $CHECKPOINT for ($EPOCHS epochs * $TOTAL_TOKENS tokens / ($SEQ_LENGTH seq length * $BATCH_SIZE batch size)) = $STEPS steps"
 
+source $HOME/venv/bin/activate
 
-python3 -m EasyLM.models.llama.llama_train \
+nohup $HOME/venv/bin/python3 -m EasyLM.models.llama.llama_train \
     --jax_distributed.initialize_jax_distributed=True \
     --mesh_dim='1,16,1' \
     --dtype='bf16' \
@@ -25,8 +36,8 @@ python3 -m EasyLM.models.llama.llama_train \
     --save_milestone_freq=10000 \
     --load_llama_config='control' \
     --update_llama_config='' \
-    --load_dataset_state='gs://mimir-train-us/llama-control-checkpoint/090ec66984374125bca9f1f490a746ca/dataset.pkl' \
-    --load_checkpoint='trainstate::gs://mimir-train-us/llama-control-checkpoint/090ec66984374125bca9f1f490a746ca/streaming_train_state' \
+    --load_dataset_state="$CHECKPOINT/dataset.pkl" \
+    --load_checkpoint="trainstate::$CHECKPOINT/streaming_train_state" \
     --tokenizer.pretrained_model_name_or_path='mimir-project/tokenizer' \
     --optimizer.type='adamw' \
     --optimizer.adamw_optimizer.weight_decay=0.0 \
@@ -62,5 +73,8 @@ python3 -m EasyLM.models.llama.llama_train \
     --logger.project="llama-control" \
     --logger.output_dir="gs://mimir-train-us/llama-control-checkpoint" \
     --logger.wandb_dir="$HOME/wandb" \
-    --logger.entity="nbailab"
+    >& "~/nohup.out" &
+
+echo "See log at ~/nohup.out"
+##    --logger.entity="nbailab"
 ##|& tee $HOME/output.txt
